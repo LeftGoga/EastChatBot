527bbf753934d9085533567c8ed1abff133a3c46
Egor Polyakov <leftgoga@yandex.ru>
2024-06-12 01:01:46 +0700

    Проблема в том, что GPT_connector принимает объект ретривер, который делается из бд-объекта. Но мы по идее должны туда передавать текст, так как его уже достали из бд и перевели. Возможно придется создавать еще одну бд чисто ради этого

--- TB.py
+++ TB.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot('7281361012:AAGTlHUgS53joaRVMzWASv-txLX1U_KZ1RM')
 con = connector('AQVNyrGYKFnMnwXWjsSQkylmRXPNo4jhc9gmGzc9', "https://translate.api.cloud.yandex.net/translate/v2/translate")
 db = DB_connector()
 
-print(db.list_collections())
+print(db.client.list_collections())
 #retriever = db.as_retriever()
 
 @bot.message_handler(content_types=['text'])
@@ -17,12 +17,16 @@ def get_text_messages(message):
     if message=='/start':
         bot.send_message(message.from_user.id, f"Yo!")
         return
+    print(message.text)
     print('Translating')
-    text_ru = con.make_requests(message.text, 'ru', 'ja')
+    text_ru = con.make_requests([message.text], 'ru', 'ja')
+    print(text_ru)
     print('Extracting prompt')
-    ans = db.query(text_ru, "test_coll")['documents'][0]
+    ans = db.query(text_ru[0], "test_coll")['documents'][0]
+    print(ans)
     print("ans")
     text_ans = con.make_requests(ans, 'ja', 'ru')
+    print(text_ans)
     print("texts_ans")
     gpt_connector = GPT_connector('AQVNyrGYKFnMnwXWjsSQkylmRXPNo4jhc9gmGzc9', text_ans, 'b1gsm96j2ptjrhcubqu4')
     gpt_recon = gpt_connector.make_requests(message.text)
